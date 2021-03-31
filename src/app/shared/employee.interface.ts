86def53fb9eb28d8cc1725f74119d82ef2eabf57
nicolebarleta <mnbarleta@my365.bellevue.edu>
2021-03-30 22:45:13 -0500

    Adding employee and item interface

--- src/app/shared/employee.interface.ts
+++ src/app/shared/employee.interface.ts
@@ -1,3 +1,14 @@
+/*
+ ============================================
+; Title:  employee.interface.ts
+; Author: Professor Krasso
+; Date: 30 March 2021
+; Modified By: Marie Nicole Barleta
+; Description: Declared variables to connect to MongoDB database
+; The employees connection
+;===========================================
+ */
+
 import { Item } from "./item.interface";
 
 export interface Employee {
--- src/app/shared/item.interface.ts
+++ src/app/shared/item.interface.ts
@@ -1,3 +1,14 @@
+/*
+ ============================================
+; Title:  item.interface.ts
+; Author: Professor Krasso
+; Date: 30 March 2021
+; Modified By: Marie Nicole Barleta
+; Description: Declared variables to connect to MongoDB database
+; The items connection, specifically the tasks
+;===========================================
+ */
+
 export interface Item {
     _id: string;
     text: string;
